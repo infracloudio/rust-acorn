4dbf53f7e3b33b875f050cbaf71ebd5255ad8186
Aashi Modi <ash007modi@gmail.com>
2023-11-08 20:58:09 +0530

    added acornfile

--- fullstack-rust-app/frontend/src/api.rs
+++ fullstack-rust-app/frontend/src/api.rs
@@ -1,8 +1,20 @@
 use common::{ErrorResponse, Feedback, FeedbackListResponse, FeedbackResponse};
 use reqwasm::http;
+//use dotenv::dotenv;
+//use std::env;
+//use lazy_static::lazy_static;
+
+// lazy_static! {
+//     static ref BACKEND_SERVER: String = env::var("BACKEND_SERVER").expect("Error: BACKEND_SERVER not found");
+// }
+
+//let backend_server: &'static str = std::env!("BACKEND_SERVER");
+
+//const backend_server = env::var("BACKEND_SERVER").expect("Error: BACKEND_SERVER not found").to_string();
 
 pub async fn api_create_feedback(feedback_data: &str) -> Result<Feedback, String> {
-    let response = match http::Request::post("http://localhost:8000/api/feedbacks/")
+    let backend_server = std::env!("BACKEND_SERVER");
+    let response = match http::Request::post(format!("https://{}/api/feedbacks/", backend_server).as_str())
         .header("Content-Type", "application/json")
         .body(feedback_data)
         .send()
@@ -29,8 +41,9 @@ pub async fn api_create_feedback(feedback_data: &str) -> Result<Feedback, String
 }
 
 pub async fn _api_fetch_single_feedback(feedback_id: &str) -> Result<Feedback, String> {
+    let backend_server = std::env!("BACKEND_SERVER");
     let response = match http::Request::get(
-        format!("http://localhost:8000/api/feedbacks/{}", feedback_id).as_str(),
+        format!("https://{}/api/feedbacks/{}", backend_server, feedback_id).as_str(),
     )
     .send()
     .await
@@ -56,12 +69,11 @@ pub async fn _api_fetch_single_feedback(feedback_id: &str) -> Result<Feedback, S
 }
 
 pub async fn api_fetch_feedbacks((page, limit): (i32, i32)) -> Result<Vec<Feedback>, String> {
+    //dotenv().ok();
+    let backend_server = std::env!("BACKEND_SERVER");
+    //let backend_server = env::var("BACKEND_SERVER").is_err();
     let response = match http::Request::get(
-        format!(
-            "http://localhost:8000/api/feedbacks?page={}&limit={}",
-            page, limit
-        )
-        .as_str(),
+        format!("https://{}/api/feedbacks?page={}&limit={}", backend_server, page, limit).as_str(),
     )
     .send()
     .await
@@ -87,8 +99,9 @@ pub async fn api_fetch_feedbacks((page, limit): (i32, i32)) -> Result<Vec<Feedba
 }
 
 pub async fn api_delete_feedback(feedback_id: &str) -> Result<(), String> {
+    let backend_server = std::env!("BACKEND_SERVER");
     let response = match http::Request::delete(
-        format!("http://localhost:8000/api/feedbacks/{}", feedback_id).as_str(),
+        format!("https://{}/api/feedbacks/{}", backend_server, feedback_id).as_str(),
     )
     .send()
     .await
